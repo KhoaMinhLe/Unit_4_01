f9978090d8f1bef58a66d8a47bb0c2b282ca442a
KhoaMinhLe <Khoa.le@ocsbstudent.ca>
2017-10-30 17:14:51 -0400

    Unit_4_01

---
+++ Unit_4_01.py
@@ -0,0 +1,8 @@
+# Created by: Khoa Le
+# Created on October 30 2017
+# Created for ICS3U
+# This program calculates celcius to fahrenheit.
+
+tc = input()
+tf = 1.8*tc+32.0
+print("Fahrenheit = " + str(tf))
